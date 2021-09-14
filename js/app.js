cef1b01db49d0a835360a478a7052ae8b4c40899
ojedam13 <m_ojeda17@hotmail.com>
2021-09-14 10:12:47 -0300

    opciones de años en js

---
+++ js/app.js
@@ -0,0 +1,33 @@
+//constructores
+function seguro(marca, year, tipo) {
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
+
+}
+
+function UI() { }
+
+//llena las opciones de los años
+UI.prototype.llenarOpciones = () => {
+    const max = new Date().getFullYear(),
+        min = max - 20;
+    
+    const selectYear = document.querySelector('#year');
+
+    for(let i = max; i > min; i--) {
+        let option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        selectYear.appendChild(option);
+    }
+}
+
+//instanciar UI
+
+const ui = new UI();
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    ui.llenarOpciones(); //llena selec con los años
+})
