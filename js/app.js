de65b1c2f05b96920c6712512af262dae992822b
ojedam13 <m_ojeda17@hotmail.com>
2021-09-14 10:21:14 -0300

    validar form

--- js/app.js
+++ js/app.js
@@ -31,3 +31,29 @@ const ui = new UI();
 document.addEventListener('DOMContentLoaded', () => {
     ui.llenarOpciones(); //llena selec con los años
 })
+
+eventListeners();
+function eventListeners() {
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarSeguro);
+}
+
+function cotizarSeguro(e) {
+    e.preventDefault();
+    //leer marca seleccionado
+    const marca = document.querySelector('#marca').value;
+
+    //leer año
+    const year = document.querySelector('#year').value;
+
+
+    //leer cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+    if (marca === '' || year === '' || tipo === '') {
+        
+    } else {
+        
+    }
+}
+
